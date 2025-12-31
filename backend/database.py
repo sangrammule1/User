b4a4b5c6e768e795a2e369d23179b9d06bd74b1d
Sangram Mule <sangrammule@Sangrams-MacBook-Air.local>
2025-12-31 13:32:32 +0530

    Apply instruction: add zipcode field to user

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "mysql+pymysql://root:password@localhost/user_db"
@@ -8,3 +8,14 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    zipcode = Column(String)
+
+Base.metadata.create_all(bind=engine)
