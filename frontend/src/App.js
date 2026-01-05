6f67b667a832efc068f6397bfacaabaeef4e12fd
Sangram Mule <sangrammule@Sangrams-MacBook-Air.local>
2026-01-05 18:39:11 +0530

    commit 15

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,7 @@ function App() {
   const [form, setForm] = useState({
     first_name: "",
     last_name: "",
-    phone: "",
-    email: ""
+    phone: ""
     });
 
   const handleChange = (e) => {
@@ -57,16 +56,6 @@ function App() {
           <input
             name="phone"
             value={form.phone}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        
-        <div >
-          <label >Email:</label > <br />
-          <input
-            name="email"
-            value={form.email}
             onChange={handleChange}
             required
           />
