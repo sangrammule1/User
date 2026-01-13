f15b4c8a649692d72665290de69dd159ee2e1065
Sangram Mule <sangrammule@Sangrams-MacBook-Air.local>
2026-01-13 18:19:52 +0530

    commit 17

--- backend/schemas.py
+++ backend/schemas.py
@@ -5,6 +5,3 @@ class UserCreate(BaseModel):
     first_name: str
     last_name: str
     phone: str
-    email: str
-    zipcode: str
-    house_no: str
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -63,36 +63,6 @@ function App() {
             required
           />
         </div>
-        
-        <div >
-          <label >Email:</label > <br />
-          <input
-            name="email"
-            value={form.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        
-        <div >
-          <label >Zipcode:</label > <br />
-          <input
-            name="zipcode"
-            value={form.zipcode}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        
-        <div >
-          <label >House No:</label > <br />
-          <input
-            name="house_no"
-            value={form.house_no}
-            onChange={handleChange}
-            required
-          />
-        </div>
         <br/>
         <button type="submit">Submit</button>
       </form>
